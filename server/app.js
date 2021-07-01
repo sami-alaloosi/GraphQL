f653ada05b371647d658d0a933378caa41f01e7e
sami-aloosi <kingxsam@gmail.com>
2021-07-01 05:45:00 -0700

    books schema

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 /** @format */
-
-
 import express from "express";
 import { graphqlHTTP } from 'express-graphql';
+import schema from "./schema/schema"
+
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+schema
 }));
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,42 @@
 /** @format */
 
 import graphql from "graphql";
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+const books = [
+  { id: "1", name: "cracking the coding interview", genre: "Programming" },
+  {
+    id: "2",
+    name: "Designing Data-Intensive Applications",
+    genre: "Programming",
+  },
+  { id: "3", name: "The 48 Laws of Power", genre: "Self-help book" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db / other sources
+        return books.find(args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
