1a80b5c96b85e9d65a6538785e0bef1404c17fcf
sami-aloosi <kingxsam@gmail.com>
2021-07-01 09:34:11 -0700

    adding dummy.js

---
+++ server/schema/dummy.js
@@ -0,0 +1,17 @@
+// this file will be deleted after creating the db
+
+export const books = [
+    { id: "1", name: "cracking the coding interview", genre: "Programming" },
+    {
+      id: "2",
+      name: "Designing Data-Intensive Applications",
+      genre: "Programming",
+    },
+    { id: "3", name: "The 48 Laws of Power", genre: "Self-help book" },
+  ];
+
+export const authors = [
+    { id: 1 ,name: "Gayle Laakmann McDowell", age: 40 },
+    { id: 2 ,name: "Martin Kleppmann", age: 33 },
+    { id: 3 ,name: "Robert Greene", age: 50 }
+]
