1b62d74172caaec2d6a5f715cb54e71dbb8f8ea8
sami-aloosi <kingxsam@gmail.com>
2021-07-02 01:44:57 -0700

    creating book and author schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 /** @format */
 import graphql from "graphql";
+import Book from "../models/book.js";
+import Author from "../models/author.js";
+import _ from "lodash";
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -8,10 +11,10 @@ const {
   GraphQLInt,
   GraphQLList
 } = graphql;
-import _ from "lodash";
 
-//dummy data
-import { books, authors } from "./dummy.js";
+
+// //dummy data
+// import { books, authors } from "./dummy.js";
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -22,7 +25,7 @@ const BookType = new GraphQLObjectType({
     author:{
       type: AuthorType,
       resolve(parent, args){
-       return  _.find(authors,{id: parent.authorId})
+      //  return  _.find(authors,{id: parent.authorId})
       }
     }
   }),
@@ -37,7 +40,7 @@ const AuthorType = new GraphQLObjectType({
     books:{
       type: new GraphQLList(BookType),
       resolve(pareant, args){
-        return _.filter(books, {authorId: pareant.id})
+        // return _.filter(books, {authorId: pareant.id})
       }
     }
   }),
@@ -51,14 +54,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other sources
-        return _.find(books, { id: args.id });
+        // return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        // return _.find(authors, { id: args.id });
       },
     },
     books: {
