b4d93bf38036e2dd44bbe1d9c9b10a0a3bc62299
sami-aloosi <kingxsam@gmail.com>
2021-07-02 01:15:59 -0700

    connecting to mongodb

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
 /** @format */
 import express from "express";
-import { graphqlHTTP } from 'express-graphql';
-import schema from "./schema/schema.js"
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema/schema.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-schema,
-graphiql: true,
-}));
+mongoose.connect(`${process.env.MONGODB}`,  {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', ()=>{
+    console.log('connected to database');
+})
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log("Listening for request on port 4000");
