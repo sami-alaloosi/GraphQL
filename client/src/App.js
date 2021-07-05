189fd0c2f120d80dad72c0ce9a2648df4c2ceea4
sami-aloosi <kingxsam@gmail.com>
2021-07-04 23:29:07 -0700

    creating booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 /** @format */
 
 import React from "react";
+import BookList from "./components/BookList";
 
 function App() {
   return <div id="main">
     <h1> Hellow World</h1>
+    <BookList/>
   </div>;
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function BookList() {
+    return (
+        <div>
+            <ul id="book-list">
+                <li>
+                    Book name
+                </li>
+            </ul>
+        </div>
+    )
+}
