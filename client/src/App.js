89952c97d825a8324c71d8f07d187e6bee9b3ba9
sami-aloosi <kingxsam@gmail.com>
2021-07-04 23:39:20 -0700

    apollo set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,28 @@
 
 import React from "react";
 import BookList from "./components/BookList";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache()
+});
 
 function App() {
-  return <div id="main">
-    <h1> Hellow World</h1>
-    <BookList/>
-  </div>;
+  return (
+    <ApolloProvider client={client}>
+    <div id="main">
+      <h1> Hellow World</h1>
+      <BookList />
+    </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
